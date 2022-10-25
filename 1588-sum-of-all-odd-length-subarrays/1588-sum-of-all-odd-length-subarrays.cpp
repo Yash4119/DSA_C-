d441fc095e73fcf3299f3b2cd9a74a1bbd0ba6a8
Yash_4119 <81731490+Yash4119@users.noreply.github.com>
2022-10-25 11:20:46 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (38.07%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
@@ -2,22 +2,30 @@ class Solution {
 public:
     int sumOddLengthSubarrays(vector<int>& arr) {
         
-        vector<int>prefix(arr.size()+1,0);
-        for(int i=1;i<=arr.size();i++){
-            prefix[i] = arr[i-1] + prefix[i-1];
-        }
+//         vector<int>prefix(arr.size()+1,0);
+//         for(int i=1;i<=arr.size();i++){
+//             prefix[i] = arr[i-1] + prefix[i-1];
+//         }
         
-        int sum=0;
+//         int sum=0;
+        
+//         for(int i=0;i<arr.size();i++){
+//             vector<int>temp;
+//             for(int j=i;j<arr.size();j++){
+//                 temp.push_back(arr[j]);
+//                 if(temp.size() % 2){
+//                   sum += (prefix[j+1] - prefix[i]);
+//                 }
+//             }
+//         }
+        
+//         return sum;
+        
+        int sum = 0,n=arr.size();
         
         for(int i=0;i<arr.size();i++){
-            vector<int>temp;
-            for(int j=i;j<arr.size();j++){
-                temp.push_back(arr[j]);
-                if(temp.size() % 2){
-                  sum += (prefix[j+1] - prefix[i]);  
-                  // cout<<i<<" "<<j<<" "<<prefix[i]<<" "<<prefix[j+1]<<endl;
-                }
-            }
+            int contri = ceil(((i+1)*(n-i))/2.0);
+            sum += (contri*arr[i]);
         }
         
         return sum;
