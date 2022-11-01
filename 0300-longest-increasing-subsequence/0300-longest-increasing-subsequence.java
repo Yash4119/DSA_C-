e097dd78854900c7f955b384a58a694311e23346
Yash_4119 <81731490+Yash4119@users.noreply.github.com>
2022-11-01 18:36:01 +0530

    Time: 309 ms (10.29%), Space: 67 MB (11.98%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,20 +1,23 @@
 class Solution {
-public:
-    int lengthOfLIS(vector<int>& nums) {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
         
-        int n = nums.size();
-        vector<int>dp(n,1);
+        List<Integer> dp = new ArrayList<Integer>(Arrays.asList(new Integer[n]));
+        Collections.fill(dp,1);
         
-        for(int i=0;i<n;i++){            
+        for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
-                
-                if(nums[i] > nums[j] && dp[i] < dp[j] + 1){
-                    dp[i] = dp[j]+1;
-                }                
+                if(nums[i] > nums[j] && dp.get(i) < dp.get(j)+1){
+                    dp.set(i,dp.get(j)+1);
+                }
             }
         }
+        int maxi = 1;
         
-        return *max_element(dp.begin(),dp.end());
+        for(int i=0;i<n;i++){
+            maxi = Math.max(maxi,dp.get(i));
+        }
         
+        return maxi;
     }
-};
+}
