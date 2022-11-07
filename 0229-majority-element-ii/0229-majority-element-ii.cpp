dd4a57d6a6ef079529d8bf3c76b1ecbae97f7b72
Yash_4119 <81731490+Yash4119@users.noreply.github.com>
2022-11-07 19:37:01 +0530

    Time: 21 ms (74.22%), Space: 15.9 MB (41.99%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,6 +1,39 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<int>ans;
+        
+        int n = nums.size()/3;
+        int freq=1,i=0;
+        
+        while(nums.size()>i){
+            // cout<<i<<" ";
+                while(i+1<nums.size() && nums[i]==nums[i+1]){
+                    freq++;
+                    i++;
+                }
+                // cout<<i<<endl;
+                if(freq>n)ans.push_back(nums[i]);
+                
+            freq=1;
+            i++;
+        }
+        
+        // if(freq>n)ans.push_back(nums[i-1]);
+        
+        return ans;
+    }
+};
+
+
+// Brute Force
+
+/*
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
         unordered_map<int,int>mp;
         for(auto it:nums){
             mp[it]++;
@@ -17,3 +50,5 @@ public:
         return ans;
     }
 };
+
+*/
