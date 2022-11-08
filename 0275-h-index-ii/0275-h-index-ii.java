841dc42839cd3908381d73eadc49bc57eef91894
Yash_4119 <81731490+Yash4119@users.noreply.github.com>
2022-11-08 20:07:10 +0530

    Time: 1 ms (27.97%), Space: 49.1 MB (92.94%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int arr [] = new int[n+1];
+        
+        for(int it:citations){
+            if(it>=n){
+                arr[n]++;
+            }
+            else{
+                arr[it]++;
+            }
+        }
+        
+        int total=0;
+        for(int i=n;i>=0;i--){
+            total += arr[i];
+            if(total >= i)return i;
+        }
+        
+        return n;
+    }
+}
